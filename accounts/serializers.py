07388f895223188023c9d463c8dd4a62356914d5
yuyakondo <yuyakondo333@gmail.com>
2025-07-24 16:07:48 +0900

    fix: Static analysis Minor fixes

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import CustomUser
-import re
+
 
 class CustomDateField(serializers.DateField):
     def to_internal_value(self, data):
@@ -8,22 +8,27 @@ class CustomDateField(serializers.DateField):
             return None
         return super().to_internal_value(data)
 
+
 class CustomRegisterSerializer(serializers.ModelSerializer):
-    password1 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
-    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
-    birthday = CustomDateField(required=False,allow_null=True)
+    password1 = serializers.CharField(
+        style={'input_type': 'password'},
+        write_only=True
+    )
+    password2 = serializers.CharField(
+        style={'input_type': 'password'},
+        write_only=True
+    )
+    birthday = CustomDateField(required=False, allow_null=True)
 
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password1', 'password2', 'birthday']
 
-    def validate_username(self, value):
-        if not re.match(r'^[\w@+.-]+$', value):
-            raise serializers.ValidationError({'username': "全角文字は使用できません。"})
-    
     def validate(self, attrs):
         if attrs['password1'] != attrs['password2']:
-            raise serializers.ValidationError({'non_field_errors': "パスワードが一致しません。"})
+            raise serializers.ValidationError({
+                'non_field_errors': "パスワードが一致しません。"
+            })
         return attrs
 
     def create(self, validated_data):
